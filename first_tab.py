74269c0d72a0065936ac1f4185f662b6e4f8d452
Debbie Choong <92681473+debbiechoong@users.noreply.github.com>
2024-11-27 22:44:06 +0800

    requirements.txt updated

--- first_tab.py
+++ first_tab.py
@@ -4,11 +4,6 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-sample_coords = [(835, 996), (1473, 1029), (1289, 1279), (604, 1228), (1375, 1654), (1386, 2019), 
-                 (1333, 2200), (1263, 2272), (1305, 2252), (694, 1805), (1460, 1870), (1450, 1864), 
-                 (1588, 1753), (1569, 2013), (1514, 1620), (1382, 2310), (944, 1506), 
-                 (1436, 1569), (664, 1340)]
-
 
 class AI_landmarks(): 
 
@@ -82,7 +77,7 @@ class AI_landmarks():
         keypoints_squeezed = np.squeeze(keypoints)
 
         keypoints_squeezed = [tuple(map(int, coord)) for coord in keypoints_squeezed]
-        print(keypoints_squeezed)
+
         
         return keypoints_squeezed
       
